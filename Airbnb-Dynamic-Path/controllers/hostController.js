4333715085a9ff0d7da904a0b66a78db26890cf7
Prabhat BK <prabhatbk4@gmail.com>
2026-02-06 23:45:07 +0545

    Editing page

--- Airbnb-Dynamic-Path/controllers/hostController.js
+++ Airbnb-Dynamic-Path/controllers/hostController.js
@@ -4,8 +4,31 @@ exports.getAddHome = (req, res, next) => {
   res.render("host/add-home.ejs", {
     pageTitle: "Register Home",
     currentPage: "addHome",
+    editing: false
   });
 };
+
+
+exports.getEditHome = (req, res, next) => {
+  console.log(req.url)
+  console.log('Params', req.params)
+  console.log('Query', req.query)
+
+  const homeId = req.params.homeId;
+  const editing = req.query.editing === 'true';
+  console.log(editing)
+  Home.findById(homeId, (home) => {
+    console.log(home)
+    res.render("host/add-home", {
+      home,
+      pageTitle: "Edit Home",
+      currentPage: "host-home",
+      editing
+    })
+  })
+
+}
+
 exports.getHostHome = (req, res, next) => {
   Home.fetchData((allHomes) => {
     Favourite.fetchFav((fav) => {
@@ -35,4 +58,4 @@ exports.postAddHome = (req, res, next) => {
   });
 };
 
-exports.postDeleteHome = (req, res, next) => {};
+exports.postDeleteHome = (req, res, next) => { };
