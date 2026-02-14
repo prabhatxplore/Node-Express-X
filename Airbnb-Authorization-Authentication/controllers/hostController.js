4853ec214c2defd3bd69d96dce571bc4256d0e5b
Prabhat BK <prabhatbk4@gmail.com>
2026-02-14 18:01:26 +0545

    added each user host home page

--- Airbnb-Authorization-Authentication/controllers/hostController.js
+++ Airbnb-Authorization-Authentication/controllers/hostController.js
@@ -1,3 +1,4 @@
+const { default: mongoose } = require("mongoose");
 const Home = require("../models/home");
 exports.getAddHome = (req, res, next) => {
 
@@ -5,31 +6,45 @@ exports.getAddHome = (req, res, next) => {
     pageTitle: "Register Home",
     currentPage: "addHome",
     editing: false,
-    isLogged: req.session.isLogged
+    isLogged: req.session.isLogged,
+    user: req.session.user
   });
 };
 
-exports.getEditHome = (req, res, next) => {
-  const homeId = req.params.homeId;
+exports.getEditHome = async (req, res, next) => {
+  const homeId = new mongoose.Types.ObjectId(req.params.homeId);
+  const userId = new mongoose.Types.ObjectId(req.session.user._id);
   const editing = req.query.editing === "true";
-  Home.findById(homeId).then(home => {
-    if (!home) {
-      return res.status(404).render("404", { pageTitle: 'Cannot found', currentPage: 'host-home', text: "Cannot find ID", description: "id is not found", isLogged: req.session.isLogged })
-    }
-    res.render("host/add-home", {
-      home,
-      pageTitle: "Edit Home",
-      currentPage: "host-home",
-      editing,
-      isLogged: req.session.isLogged
-    });
+
+  console.log(homeId)
+  const homeEdit = await Home.findOne({
+    _id: homeId,
+    owner: userId
+  })
+
+  if (!homeEdit) {
+    return res.status(404).render("404", { pageTitle: 'Cannot found', currentPage: 'host-home', text: "Cannot find ID", description: "id is not found", isLogged: req.session.isLogged, user: req.session.user })
+  }
+  res.render("host/add-home", {
+    home: homeEdit,
+    pageTitle: "Edit Home",
+    currentPage: "host-home",
+    editing,
+    isLogged: req.session.isLogged,
+    user: req.session.user
   });
+
 };
 
 exports.postEditHome = (req, res, next) => {
   const { _id, house_name, price, location, image_url } = req.body;
+  const homeId = new mongoose.Types.ObjectId(_id);
+  const userId = new mongoose.Types.ObjectId(req.session.user._id);
 
-  Home.findById(_id).then((home) => {
+  Home.findOne({
+    _id: homeId,
+    owner: userId
+  }).then((home) => {
     home.house_name = house_name;
     home.price = price;
     home.location = location;
@@ -47,31 +62,42 @@ exports.postEditHome = (req, res, next) => {
 };
 
 exports.getHostHome = (req, res, next) => {
-  Home.find().then((registeredHomes) => {
+
+  Home.find({ owner: req.session.user._id }).then((registeredHomes) => {
     res.render("host/host-home.ejs", {
       registeredHomes,
       pageTitle: "Host Home",
       currentPage: "host-home",
-      isLogged: req.session.isLogged
+      isLogged: req.session.isLogged,
+      user: req.session.user
     });
   })
 };
 
 exports.postAddHome = (req, res, next) => {
   const { house_name, price, location, image_url } = req.body;
-  const home = new Home({ house_name, price, location, image_url, description: "" });
+  const home = new Home({
+    house_name,
+    price,
+    location,
+    image_url,
+    description: "",
+    owner: req.session.user._id
+  });
   home.save().then(() => {
     console.log('Home save success');
     res.render("host/home-added", {
       pageTitle: "Success",
       currentPage: "addHome",
-      isLogged: req.session.isLogged
+      isLogged: req.session.isLogged,
+      user: req.session.user
     });
   });
 };
 
 exports.postDeleteHome = (req, res, next) => {
   // console.log(req.params.homeId);
+  
   Home.findOneAndDelete({ _id: req.params.homeId })
     .then(() => {
       res.redirect("/host/host-home");
