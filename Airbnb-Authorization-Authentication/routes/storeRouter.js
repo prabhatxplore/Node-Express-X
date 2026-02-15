e28a8850dc73dd39d776c11c970c38a5892ce1eb
Prabhat BK <prabhatbk4@gmail.com>
2026-02-15 19:27:36 +0545

    fix the auth error

--- Airbnb-Authorization-Authentication/routes/storeRouter.js
+++ Airbnb-Authorization-Authentication/routes/storeRouter.js
@@ -1,21 +1,19 @@
 const express = require("express");
 const homeRouter = express.Router();
 const {
-  getHomeList,
   getBookings,
   getFavouriteList,
   getHomeDetails,
   postFavouriteToggle,
 } = require("../controllers/storeController.js");
-const { isAuth } = require("../middlewares/isAuth.js");
 
-homeRouter.get("/", getHomeList);
-homeRouter.get("/bookings", isAuth, getBookings);
+
+homeRouter.get("/bookings", getBookings);
 // easier if i pass the getHome List
-homeRouter.get("/fav-list", isAuth, getFavouriteList);
+homeRouter.get("/fav-list", getFavouriteList);
 homeRouter.get("/home-details/:homeId", getHomeDetails);
 
 // homeRouter.post("/fav-list", postAddFavourite);
-homeRouter.post("/fav-list/toggle", isAuth, postFavouriteToggle);
+homeRouter.post("/fav-list/toggle", postFavouriteToggle);
 
 module.exports = homeRouter;
